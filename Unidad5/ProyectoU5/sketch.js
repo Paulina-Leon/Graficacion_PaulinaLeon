3fd16cc903cd1ece2a291ec940ab81053ef3c632
Paulina-Leon <L24170598@culiacan.tecnm.mx>
2026-05-29 07:04:41 +0000

    khbhj

--- Unidad5/ProyectoU5/sketch.js
+++ Unidad5/ProyectoU5/sketch.js
@@ -1,166 +1,107 @@
-let angulo = 0;
-
-let estrellas = [];
-
-let fondoR = 15;
-let fondoG = 10;
-let fondoB = 35;
-
-function setup() {
-
-    createCanvas(800, 600, WEBGL);
-
-    // estrellas
-    for(let i = 0; i < 200; i++){
-
-        estrellas.push({
-            x: random(-1000,1000),
-            y: random(-1000,1000),
-            z: random(-1000,1000),
-            size: random(1,4)
-        });
-    }
+let moverLuz = true; 
+let boids = []; 
+function setup() { 
+createCanvas(1200, 700, WEBGL); 
+for(let i=0;i<80;i++){ 
+boids.push({ 
+x: random(width), 
+y: random(height), 
+vx: random(-1,1), 
+vy: random(-1,1) 
+}); } 
+} 
+function draw() { 
+background(180,0,255)
+orbitControl(); // control de cámara
+ambientLight(50); // iluminación 
+directionalLight( 
+255,255,255, 
+1,1,-1 
+); 
+for(let b of boids){ 
+b.x += b.vx; 
+b.y += b.vy; 
+if(b.x<0) b.x=width; 
+if(b.x>width) b.x=0; 
+if(b.y<0) b.y=height; 
+if(b.y>height) b.y=0; 
+fill(100,255,200); 
+circle(b.x,b.y,6); 
+} 
+if(moverLuz){ // luz dinámica 
+pointLight( 
+      255,220,220, 
+      mouseX - width/2, 
+      mouseY - height/2, 
+      300 
+    ); 
+  } 
+  else { 
+    pointLight( 
+      255,220,220, 
+      0,-200,300 
+    ); 
+  } 
+  push(); // piso 
+  rotateX(HALF_PI); 
+  ambientMaterial(255,255,0); 
+  plane(1200,1200); 
+  pop(); 
+  push(); // esfera animada 
+  let x = sin(frameCount * 0.02) * 300; 
+    translate(x,0,0); 
+  specularMaterial(255); 
+  shininess(100); 
+  ambientMaterial(255,0,255);
+  sphere(90);
+  pop(); 
+// cubo rotando 
+// escala
+let s = 1 + 0.5* sin(frameCount * 0.05);
+push();
+rotate (frameCount* 0.03);
+scale(s);
+fill(100, 200, 255);
+push(); 
+translate(-250,-100,-200); 
+rotateX(frameCount * 0.01); 
+rotateY(frameCount * 0.02); 
+ambientMaterial(0,0,255);
+box(150); 
+pop(); 
+// toroide 
+push(); 
+translate(250,120,150); 
+rotateZ(frameCount * 0.03); 
+normalMaterial(); 
+torus(100,25); 
+pop(); 
+// cono 
+push(); 
+translate(0,-180,0); 
+rotateY(frameCount * 0.02);
+ambientMaterial(255,165,0); // naranja
+cone(80,180); 
+pop(); 
+// texto 
+push(); 
+resetMatrix(); 
+fill(255); 
+textSize(20); 
+text( 
+"ESCENA 3D INTERACTIVA", 
+20, 
+30 
+); 
+text( 
+"Presiona L para cambiar luz", 
+20, 
+60 
+); 
+pop(); 
 }
-
-function draw() {
-
-    background(fondoR, fondoG, fondoB);
-
-    // mover camara
-    orbitControl();
-
-
-    // 🌌 LUCES
-
-    // luz ambiental
-    ambientLight(80);
-
-    // luz direccional azul
-    directionalLight(150, 180, 255, 1, 1, -1);
-
-    // luz rosa
-    pointLight(255, 100, 200, 0, 0, 300);
-
-    // luz morada
-    pointLight(180, 100, 255, -300, -200, 200);
-
-
-    // ✨ ESTRELLAS
-    push();
-
-    noStroke();
-
-    for(let e of estrellas){
-
-        push();
-
-        translate(e.x, e.y, e.z);
-
-        ambientMaterial(255,255,255);
-
-        sphere(e.size);
-
-        pop();
-    }
-
-    pop();
-
-
-
-    // 💖 ESFERA ROSA
-    push();
-
-    rotateY(angulo);
-
-    translate(-220, 0, 0);
-
-    specularMaterial(255,150,220);
-
-    sphere(80);
-
-    pop();
-
-
-
-    // 💙 TOROIDE AZUL
-    push();
-
-    rotateX(angulo);
-
-    rotateY(angulo);
-
-    translate(220, 0, 0);
-
-    ambientMaterial(150,200,255);
-
-    torus(80,25);
-
-    pop();
-
-
-
-    // 💜 CONO CENTRAL
-    push();
-
-    rotateZ(angulo);
-
-    rotateX(angulo);
-
-    normalMaterial();
-
-    cone(90,160);
-
-    pop();
-
-
-
-    // 🌸 MINI ESFERAS FLOTANTES
-    for(let i = 0; i < 6; i++){
-
-        push();
-
-        rotateY(angulo + i);
-
-        translate(0, sin(frameCount * 0.02 + i) * 100, 250);
-
-        ambientMaterial(255,180,220);
-
-        sphere(20);
-
-        pop();
-    }
-
-
-
-    // 🌙 PISO GALAXIA
-    push();
-
-    rotateX(HALF_PI);
-
-    translate(0,300,0);
-
-    ambientMaterial(80,50,120);
-
-    plane(2000,2000);
-
-    pop();
-
-
-
-    // animacion
-    angulo += 0.01;
-}
-
-
-
-// 🎮 INTERACCION TECLADO
-function keyPressed(){
-
-    // cambiar colores fondo
-    if(key === 'F'){
-
-        fondoR = random(0,50);
-        fondoG = random(0,50);
-        fondoB = random(40,100);
-    }
-}
+function keyPressed(){ 
+if(key === 'L' || key === 'l'){ 
+moverLuz = !moverLuz;
+} 
+} 
